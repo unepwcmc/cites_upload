04c422edd8e6657bae1acd7ae9d1da479ce872bc
Simao Belchior de Castro <simaobelchior@gmail.com>
2011-12-09 11:31:02 +0000

    created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitesUpload::Application.config.secret_token = '5774ac35bc404dd117bc667e027fb4dedca0765999e41488e80f50e3552de7778f38bf465580bc4eee3476e0a5b5e85be026f19611d6c371e8ba5c3d1432df4d'
