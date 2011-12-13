45cdb94e82bf1d21e3f3780794ade8d8d0b04215
Simao Belchior de Castro <simaobelchior@gmail.com>
2011-12-13 15:18:52 +0000

    small fixes

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -8,6 +8,6 @@ module ReportsHelper
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render("uploaded_files/uploaded_file_fields", :f => builder)
     end
-    link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')", :class => "btn")
+    "#{link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')", :class => "btn add_file")}<p class='clear' />".html_safe
   end
 end
