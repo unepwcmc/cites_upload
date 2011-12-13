100b08cef8e98fb878c001aa835c45fa7ab5d9f3
Simao Belchior de Castro <simaobelchior@gmail.com>
2011-12-13 11:04:24 +0000

    adding support for I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale].presence||"en"
+  end
+
+  def default_url_options(options={})
+    options.merge({:locale => I18n.locale})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CitesUpload::Application.routes.draw do
-  resources :reports
+  scope "(:locale)", :locale => /en|fr|es/ do
+    resources :reports
+  end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
