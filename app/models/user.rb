e49632490d56ec5d90da1263cdaa729276559a4e
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-06-14 15:54:58 +0100

    Added country to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :approved, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :approved, :name, :country
 
   has_many :reports
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120614133459) do
+ActiveRecord::Schema.define(:version => 20120614143459) do
 
   create_table "reports", :force => true do |t|
     t.string   "country"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(:version => 20120614133459) do
     t.datetime "created_at",                                               :null => false
     t.datetime "updated_at",                                               :null => false
     t.string   "name"
+    t.string   "country"
   end
 
   add_index "users", ["approved"], :name => "index_users_on_approved"
