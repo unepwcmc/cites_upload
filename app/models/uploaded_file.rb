cc4a474800e167feff69697dda82fd58f517b850
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-06-15 13:58:20 +0100

    different buckets for different stages of the app

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -7,7 +7,7 @@ class UploadedFile < ActiveRecord::Base
   has_attached_file :document,
     :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/s3_storage.yml",
-    :bucket => "cites_uploaded_annual_reports"
+    :bucket => "cites_uploaded_annual_reports_#{Rails.env}"
   validates_attachment_size :document, :less_than => 10.megabytes if :document
   belongs_to :report
 end
