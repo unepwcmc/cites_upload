e881bd4766294987cdd100d919151cc512422991
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-06-20 15:53:07 +0100

    text change on the submission dialog

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(:version => 20120614170012) do
   create_table "reports", :force => true do |t|
     t.integer  "year"
     t.integer  "basis"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.boolean  "has_exports"
     t.boolean  "has_imports"
     t.boolean  "has_additional_information"
@@ -74,8 +74,8 @@ ActiveRecord::Schema.define(:version => 20120614170012) do
   create_table "uploaded_files", :force => true do |t|
     t.integer  "file_type"
     t.integer  "report_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
     t.string   "document_file_name"
     t.string   "document_content_type"
     t.integer  "document_file_size"
@@ -83,19 +83,19 @@ ActiveRecord::Schema.define(:version => 20120614170012) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "",    :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "approved",                              :default => false, :null => false
-    t.datetime "created_at",                                               :null => false
-    t.datetime "updated_at",                                               :null => false
+    t.boolean  "approved",               :default => false, :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "name"
     t.string   "country"
   end
