1ebc6a520fbdb6f2f05d0161b2cf007a9248096a
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-06-27 16:29:52 +0100

    still trying

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
   before_filter :set_locale
   before_filter :authenticate
+  protect_from_forgery
 
   private
   def set_locale
