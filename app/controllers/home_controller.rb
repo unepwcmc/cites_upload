82f65c2dbe53cd4783061d43138a586ae2f0a87e
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-07-05 16:57:26 +0100

    Explain the home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   skip_before_filter :authenticate
 
+  #Home controller exists to display information about the tool. But for now it just redirects to either
+  #the reports index page or the user sign in page, depending on authentication being present or not
   def index
     if current_admin || current_user
       redirect_to reports_path
