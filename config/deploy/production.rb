f928a63daae8f8ec343957f7f0a886480a957ccf
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-09-19 14:56:42 +0100

    Updated deploy scripts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,15 @@
 set :rails_env, "production"
 
 # Primary domain name of your application. Used in the Apache configs
-set :domain, "unepwcmc-013.vm.brightbox.net"
+set :domain, "unepwcmc-014.vm.brightbox.net"
 
 ## List of servers
-server "unepwcmc-013.vm.brightbox.net", :app, :web, :db, :primary => true
+server "unepwcmc-014.vm.brightbox.net", :app, :web, :db, :primary => true
+
+set :default_environment, {
+'PATH' => "/home/rails/.rvm/gems/ruby-1.9.2-p320/bin:/home/rails/.rvm/bin:/home/rails/.rvm/rubies/ruby-1.9.2-p320/bin:$PATH",
+'RUBY_VERSION' => 'ruby-1.9.2-p320',
+'GEM_HOME' => '/home/rails/.rvm/gems/ruby-1.9.2-p320',
+'GEM_PATH' => '/home/rails/.rvm/gems/ruby-1.9.2-p320',
+}
+
